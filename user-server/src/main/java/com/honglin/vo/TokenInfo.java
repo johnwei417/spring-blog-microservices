52801c4d532ab80ed03549d321eb333f8e2fb334
John Wei <41243908+johnwei417@users.noreply.github.com>
2020-08-19 02:30:46 -0400

    fixed TokenInfo.java

--- user-server/src/main/java/com/honglin/vo/TokenInfo.java
+++ user-server/src/main/java/com/honglin/vo/TokenInfo.java
@@ -7,6 +7,6 @@ import lombok.Data;
 public class TokenInfo {
     private String access_token;
     private String token_type;
-    private String expire_in;
+    private String expires_in;
     private String scope;
 }
